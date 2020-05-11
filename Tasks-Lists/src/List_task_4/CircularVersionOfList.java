2162b7c2d26ff14bf17b720f8be0175fdd95b8e8
Ovsiannikov78 <seri_tanya@mail.ru>
2020-05-11 10:37:46 +0200

    Tasks with Lists. Modified solution for Task_4

--- Tasks-Lists/src/List_task_4/CircularVersionOfList.java
+++ Tasks-Lists/src/List_task_4/CircularVersionOfList.java
@@ -1,35 +1,22 @@
 package List_task_4;
+
 import java.util.Collections;
 import java.util.List;
 
 public class CircularVersionOfList {
-  /*  4. Есть два списка с буквами. Определить, является ли один список циклической версией другого.
-         Например, для списков {aa, bb, cc, dd} и {cc, dd, aa, bb} результат будет true,
-         а для {dd, ee, ff} и {dd, ff, ee} - false.
-   */
-  public static void main(String[] args) {
+    /*  4. Есть два списка с буквами. Определить, является ли один список циклической версией другого.
+           Например, для списков {aa, bb, cc, dd} и {cc, dd, aa, bb} результат будет true,
+           а для {dd, ee, ff} и {dd, ff, ee} - false.
+     */
+    public static void main(String[] args) {
 
-  }
+    }
 
-  /*public static boolean circularVersionOfList(List<String> list1,List<String> list2){
-      int count = list1.size();
-      if(list1.size() == list2.size()) {
-          while (count != 0) {
-              Collections.rotate(list1, 1);
-              count--;
-              if(list1.equals(list2)) {
-                  return true;
-              }
-          }
-      }
-      return false;
-  } */
-    public static boolean circularVersionOfList(List<String> list1,List<String> list2){
-
-        if(list1.size() == list2.size()) {
-            for (int i = list1.size(); i >= 0 ; i--) {
+    public static boolean circularVersionOfList(List<String> list1, List<String> list2) {
+        if (list1.size() == list2.size()) {
+            for (int i = list1.size(); i >= 0; i--) {
                 Collections.rotate(list1, 1);
-                if(list1.equals(list2)) {
+                if (list1.equals(list2)) {
                     return true;
                 }
             }
