becd392758381d868ee9b516fbe4a0326982b9a7
Ovsiannikov78 <seri_tanya@mail.ru>
2020-06-08 10:31:55 +0200

    Unresolved two tasks

---
+++ Functional_interfaces_with_lambda_expressions/src/Task_SalaryOfEmployee/Employee.java
@@ -0,0 +1,15 @@
+package Task_SalaryOfEmployee;
+
+public class Employee {
+    private String name;
+    private int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+
+    }
+    public int getSalary() {
+        return salary;
+    }
+}
