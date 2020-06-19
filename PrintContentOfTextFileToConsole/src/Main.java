2b382a8d5c60df4951cfc9a725f510815cec79d4
Ovsiannikov78 <seri_tanya@mail.ru>
2020-06-19 13:23:19 +0200

    Completed task - Print_a_content_of_a_text_file_to_console

--- PrintContentOfTextFileToConsole/src/Main.java
+++ PrintContentOfTextFileToConsole/src/Main.java
@@ -3,18 +3,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
-
+import java.util.stream.Stream;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        File file = new File("data.txt");
-
-        getContentFromTextFile(file);
+    public static void main(String[] args) {
+        String data = args[0];
+        getContentFromTextFile(data);
     }
-    public static void getContentFromTextFile(File file) throws IOException {
-         Collection<String> lines = Files.readAllLines(Paths.get(String.valueOf(file)) );
+    public static void getContentFromTextFile(String data){
+        try {
+            Stream<String> lines = Files.lines(Paths.get(data));
             lines.forEach(System.out::println);
+        }catch (IOException e){
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+
     }
 }
 
