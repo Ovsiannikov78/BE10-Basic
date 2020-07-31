582d3e036e3effd44f107a594f25f6473fff83b2
Ovsiannikov78 <seri_tanya@mail.ru>
2020-07-31 17:29:46 +0200

    redone for general case

--- HW-27-07-2020-Tasks_with_the_Strings/src/Task_2_ToUpperAndToLowerCase/ToUpperAndToLowerCase.java
+++ HW-27-07-2020-Tasks_with_the_Strings/src/Task_2_ToUpperAndToLowerCase/ToUpperAndToLowerCase.java
@@ -1,9 +1,15 @@
 package Task_2_ToUpperAndToLowerCase;
 
+import static java.util.stream.Collectors.joining;
+
 public class ToUpperAndToLowerCase {
     public static void main(String[] args) {
         String str = "Hel*lo";
+        String str1 = "hELLOwORLD";
+
         System.out.println(stringTransformationByRequest(str));
+        System.out.println("================================");
+        System.out.println(stringTransformationByRequest1(str1));
 
     }
     /*
@@ -11,8 +17,19 @@ public class ToUpperAndToLowerCase {
        следующим образом, заглавные буквы становятся маленькими,
        а маленькие - заглавными ; Hel*lo -> hEL*LO
      */
-    public static String stringTransformationByRequest(String str) {
-        return new StringBuilder(str).substring(0, 1).toLowerCase() + str.substring(1, 6).toUpperCase();
 
+
+    public static String stringTransformationByRequest(String str) {
+        return new StringBuilder(str).substring(0, 1).toLowerCase() + str.substring(1, 6).toUpperCase(); // для конкретной строки
+
+    }
+
+    public static String stringTransformationByRequest1(String str1) {
+        return str1
+                .chars()
+                .map(s -> Character.isUpperCase(s) ? Character.toLowerCase(s) : Character.toUpperCase(s))
+                .mapToObj(s -> (char) s)
+                .map(Object::toString)
+                .collect(joining());
     }
 }
--- HW-27-07-2020-Tasks_with_the_Strings/src/Task_2_ToUpperAndToLowerCase/ToUpperAndToLowerCaseTest.java
+++ HW-27-07-2020-Tasks_with_the_Strings/src/Task_2_ToUpperAndToLowerCase/ToUpperAndToLowerCaseTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class ToUpperAndToLowerCaseTest {
     String str = "Hel*lo";
+    String str1 = "hELLOwORLD";
         /*
            Есть строка. Вернуть новую строку, полученную из первой
            следующим образом, заглавные буквы становятся маленькими,
@@ -23,6 +24,21 @@ public class ToUpperAndToLowerCaseTest {
     public void incorrectlyConvertedString() {
         String exp = "HEL*lo";
         String akt = ToUpperAndToLowerCase.stringTransformationByRequest(str);
+
+        Assert.assertNotEquals(exp, akt);
+    }
+    @Test
+    public void correctlyConvertedString1() {
+        String exp = "HelloWorld";
+        String akt = ToUpperAndToLowerCase.stringTransformationByRequest1(str1);
+
+        Assert.assertEquals(exp, akt);
+    }
+
+    @Test
+    public void incorrectlyConvertedString1() {
+        String exp = "HelloWORLD";
+        String akt = ToUpperAndToLowerCase.stringTransformationByRequest1(str1);
 
         Assert.assertNotEquals(exp, akt);
     }
